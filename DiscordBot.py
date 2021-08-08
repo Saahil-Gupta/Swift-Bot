b938c083917bc9d11a61dad531bfd2f900f0808b
Saahil-Gupta <83634729+Saahil-Gupta@users.noreply.github.com>
2021-08-08 22:57:18 +0530

    ChangeLog V1.2
    Added Autofeeds and emojis to it.

--- DiscordBot.py
+++ DiscordBot.py
@@ -3,7 +3,12 @@ import os
 from discord.ext import commands
 from discord.utils import get
 from discord import Embed, DMChannel, Role, Attachment
-
+from discord.abc import GuildChannel, _Overwrites
+import asyncio
+import datetime as dt
+from discord.ext import tasks
+import asyncio
+from asyncio import tasks
 bot = commands.Bot(command_prefix='!jb')
 
 # ----------------------------------------------------------------------------------------------
@@ -13,6 +18,19 @@ bot = commands.Bot(command_prefix='!jb')
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name='DM to Contact Staff | !jbhelp'))
     print('Bot is online and Ready to Go')
+    msg1.start()
+
+@tasks.loop(hours=8)
+async def msg1():
+    message_channel = bot.get_channel(748798206816813121) # • 748798206816813121
+    pink_heart = discord.utils.get(bot.emojis, id=680554415534768131)
+    pink_butterfly = discord.utils.get(bot.emojis, id=856262693241749525)
+    embedAuto = discord.Embed(title=f'{pink_butterfly} Sub Reminder',description=f'\n{pink_heart} TYSM for all the support!\n\n'
+    "• Sub to [Julia Burch](https://www.youtube.com/channel/UCcRKWdzb5P9jsswSHmuq9PA)\n"
+    "• Sub to [Julia Burch Shorts](https://www.youtube.com/channel/UCpaxSWzKLtS3uY4Gkp-_SOQ)\n"
+    "• Sub to [Julia Burch Livestreams](https://www.youtube.com/channel/UCigOCMIzDMXGJXtGOQZv6xA)",color=0xFFC1E6)
+    embedAuto.set_thumbnail(url="https://images-ext-2.discordapp.net/external/otj151m1XhrpCs_OUT2SIhBHTXpa4P9hLtZjUOQEWLI/%3Fv%3D1/https/cdn.discordapp.com/emojis/850562956877365298.gif")
+    await message_channel.send(embed=embedAuto)
 
 
 @bot.command()
